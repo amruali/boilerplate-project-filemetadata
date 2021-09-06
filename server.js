11f278e1f10abf405ae807f3246219f5fc7afdce
Amr Ali <amrali90111@gmail.com>
2021-09-06 10:38:11 +0000

    ...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,6 +12,45 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
+// The most important two lines 
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+
+
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+    //console.log(req.file);
+
+    if(!req.file){
+        next();
+    }
+    console.log(req.file)
+
+    result = {
+        name : req.file.originalname,
+        type : req.file.mimetype,
+        size : req.file.size
+    }
+
+    console.log(result)
+    res.json(result)
+})
 
 
 
